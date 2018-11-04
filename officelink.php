f65d5e021806f5603a8398e9c80a179cac7cdf2c
NCVIM <42138298+NCVIM@users.noreply.github.com>
2018-11-04 17:06:53 +0530

    Add files via upload

---
+++ officelink.php
@@ -0,0 +1,441 @@
+<!DOCTYPE HTML>
+
+
+<html>
+
+<head>
+	<title>OFFICE LINK</title>
+  <link rel="stylesheet" type="text/css" href="assets/login/css/util.css">
+  <link rel="stylesheet" type="text/css" href="assets/login/css/main.css">
+
+
+
+  <link rel="stylesheet" href="assets/css/ma.css" /> 
+
+</head>
+
+<body>
+  <?php
+
+
+  include_once 'connection.php';
+  $id=3;
+  ?>
+
+
+  <!-- Header -->
+
+  <header id="header">
+
+    <div class="inner">
+
+      <a href="index.php" class="logo">Aqua Loom</a>
+
+
+      <nav id="nav">
+
+        <a href="index.php">Home</a>
+
+        <a href="login.php">login</a>
+
+        <a href="#####">contact us</a>
+
+      </nav>
+
+    </div>
+
+  </header>
+
+  <a href="#menu" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+
+
+  <!-- Main -->
+
+
+  <div class="limiter">
+    <div class="container-login100">
+     <div class="wrap-login100">
+      <div class="login100-form-title" style="background-image: url(assets/login/images/bg-01.jpg);">
+       <span class="login100-form-title-1">
+        OFFICE LINK
+      </span>
+    </div>
+    <!-- Form for Adding details -->
+
+    <form id="add"action="" method="post">
+
+
+      <table style="width:100%">
+
+        <!-- Selecting MD --> 
+        <tr>
+         <td><label for="Superior">Select Superior: </label></td>
+         <?php
+
+         $m="select * from office where O_Id=1";
+         $result=mysqli_query($conn,$m);
+         ?>
+
+         <td> <select name="md" id="md"  style=" height: 50px; width: 150px;">
+           <option value="">Select MD</option>
+           <?php
+           
+           while($row=mysqli_fetch_array($result))
+           {
+            echo"<option value='". $row['O_Id']."'>" . $row['O_Name'] . "</option>";
+          }
+
+          echo "</select>"       
+          ?>
+
+        </td>
+
+        <td><label for="District">Select District: </label></td>
+        <?php
+
+        $m="select * from district";
+        $result=mysqli_query($conn,$m);
+        ?>
+
+        <td> <select name="District"  id="district" style=" height: 50px; width: 150px;">
+         <option value="" selected disabled>Select District</option>
+         <?php
+         $District=$_POST['District'];
+         while($row=mysqli_fetch_array($result))
+         {
+          echo"<option value='". $row['District_Id']."'>" . $row['District_Name'] . "</option>";
+        }
+
+        echo "</select>"
+        ?>
+
+      </td>
+
+    </tr>
+
+    <!-- Selecting CE --> 
+    <tr>
+      <td><label for="ce">Select CE : </label></td>
+      <td><select name="ce" id="ce" style=" height: 50px; width: 150px;">
+       <option value="">Select MD first</option></td>
+
+       <td><label for="taluk">Select Taluk : </label></td>
+       <td><select name="taluk" id="taluk">
+         <option value="">Select District first</option></td>
+
+
+       </tr>
+
+       <!-- Selecting SE --> 
+       <tr>
+        <td><label for="se">Select SE : </label></td>
+        <td><select name="se" id="se" style=" height: 50px; width: 150px;">
+         <option value="">Select CE first</option></td>
+
+         <td><label for="panchayat">Select Panchayat : </label></td>
+         <td>
+          <!--<div id="checkboxes">
+           <input type="checkbox" name="check" ><br>
+         </div></td>  
+         <input type="text" placeholder="SELECT PANCHAYATH" name="us" required>  </td><td>-->   <div class="multiselect">
+    <div class="selectBox" onclick="showCheckboxes()">
+      <select>
+        <option>Select Panchayath</option>
+		
+      </select>
+      <div class="overSelect"></div>
+    </div>
+    <!--<div id="checkboxes">-->
+    <div id="checkboxes">
+           <input type="checkbox" name="check" ><br>
+         
+      <!--<label for="one">
+        <input type="checkbox" id="one" />First checkbox</label>
+      <label for="two">
+        <input type="checkbox" id="two" />Second checkbox</label>
+      <label for="three">
+        <input type="checkbox" id="three" />Third checkbox</label>-->
+    </div>
+  </div> </td></tr>
+         
+
+       </tr>
+
+       <!-- Selecting EE --> 
+       <tr>
+        <td><label for="ee">Select EE : </label></td>
+        <td><select name="ee" id="ee" style=" height: 50px; width: 150px;">
+         <option value="">Select SE first</option></td>
+
+       </tr>
+
+       <!-- Selecting AXE --> 
+       <tr>
+        <td><label for="axe">Select AXE : </label></td>
+        <td><select name="axe" id="axe" style=" height: 50px; width: 150px;">
+         <option value="">Select EE first</option></td>
+
+       </tr>
+
+
+       <!-- Selecting AE --> 
+       <tr>
+        <td><label for="ae">Select AE : </label></td>
+        <td><select name="ae" id="ae" style=" height: 50px; width: 150px;">
+         <option value="">Select AXE first</option></td>
+         <td><input type="submit" value="ADD" name="submit" class="alt" /></td>
+
+       </tr>
+
+
+
+
+
+
+     </table>    
+
+
+
+
+
+
+     <!-- Scripts -->
+			<!-- <script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+      <script src="assets/js/main.js"></script></script> -->
+
+      <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+
+
+      <script type="text/javascript">
+        $(document).ready(function()
+        {
+          $(document).on('change', '#md', function()
+          { 
+            var mdID = $(this).val();
+            if(mdID){
+              $.ajax({
+                type:'POST',
+                url:'officelinkajax.php',
+                data:'md_Id='+mdID,
+                success:function(response){ 
+                  $('#ce').html(response);
+
+                }
+              }); 
+            }else{
+              $('#ce').html('<option value="">Select MD</option>');
+              $('#se').html('<option value="">Select SE</option>');
+              $('#ee').html('<option value="">Select EE</option>');
+              $('#axe').html('<option value="">Select AXE</option>');
+
+            }
+          });
+
+
+
+
+          $(document).on('change', '#ce', function()
+          { 
+            var ce = $(this).val();
+            if(ce){
+              $.ajax({
+                type:'POST',
+                url:'officelinkajax.php',
+                data:'CE='+ce,
+                success:function(response){ 
+                  $('#se').html(response);
+                }
+              }); 
+            }else{
+              $('#se').html('<option value="">Select SE</option>');
+              $('#ee').html('<option value="">Select EE</option>');
+              $('#axe').html('<option value="">Select AXE</option>');
+
+            }
+          });
+
+
+          
+          $(document).on('change', '#se', function()
+          { 
+            var se = $(this).val();
+            if(se){
+              $.ajax({
+                type:'POST',
+                url:'officelinkajax.php',
+                data:'SE='+se,
+                success:function(response){ 
+                  $('#ee').html(response);
+                }
+              }); 
+            }else{
+
+              $('#ee').html('<option value="">Select EE</option>');
+              $('#axe').html('<option value="">Select AXE</option>');
+
+            }
+          });
+
+
+          $(document).on('change', '#ee', function()
+          { 
+            var ee = $(this).val();
+            if(ee){
+              $.ajax({
+                type:'POST',
+                url:'officelinkajax.php',
+                data:'EE='+ee,
+                success:function(response){ 
+                  $('#axe').html(response);
+                }
+              }); 
+            }else{
+
+              $('#exe').html('<option value="">Select EE</option>');
+              
+            }
+          });
+
+
+          $(document).on('change', '#axe', function()
+          { 
+            var axe = $(this).val();
+            if(axe){
+              $.ajax({
+                type:'POST',
+                url:'officelinkajax.php',
+                data:'AXE='+axe,
+                success:function(response){ 
+                  $('#ae').html(response);
+                }
+              }); 
+            }else{
+
+              $('#ae').html('<option value="">Select EE</option>');
+              
+            }
+          });
+
+          $(document).on('change', '#district', function(){ 
+            var districtID = $(this).val();
+            if(districtID){
+              $.ajax({
+                type:'POST',
+                url:'officelinkajax.php',
+                data:'district_id='+districtID,
+                success:function(response){ 
+                  $('#taluk').html(response);
+
+                }
+              }); 
+            }else{
+              $('#taluk').html('<option value="">Select District</option>');
+
+            }
+          });
+
+
+
+          $(document).on('change','#taluk',function(){
+           var TalukID = $(this).val();
+           if(TalukID){
+            $.ajax({
+             type:'POST',
+             url:'officelinkajax.php',
+             data:'Taluk_ID='+TalukID,
+             success:function(html){
+               console.log(html);
+               $('#checkboxes').html(html);
+             }
+           }); 
+          }else{
+           $('#check').html('<option value="">Select Taluk first</option>'); 
+         }
+       });
+
+
+
+
+
+          $('#add').submit(function(event) {
+
+           event.preventDefault();
+           $check = $('#check').val();
+           if($ae == null) {
+            alert(" select Taluk first !");
+            return;
+          }
+          $check = $('#check').val();
+          $check = $check.trim();
+          if( $ae.length < 1) {
+            alert(" Select a valid Panchayat");
+            return;
+          }
+          
+          $.ajax({
+            type:'POST',
+            url:'officelinkajax.php',
+            data: $(this).serialize(),
+            success:function(response){
+              console.log(response);
+              switch(parseInt(response.trim())) {
+                case 1: 
+                {
+                  alert("data inserted successfully");
+                  $('#md').val('');
+                  $('#ce').val('');
+                  $('#se').val('');
+                  $('#ee').val('');
+                  $('#axe').val('');
+                  $('#ae').val('');
+
+                  break;
+                }
+                case 2:
+                {
+                  alert("Duplicate entry");
+
+                  break;
+                }
+                default:
+                alert(" Errror from server");
+              }
+
+            }
+          }); 
+          
+
+
+
+
+        });
+
+
+        });
+        {
+
+var expanded = false;
+
+function showCheckboxes() {
+
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+
+
+}
+      </script>
+
+
+
+
+    </body>
+    </html>
---
+++ officelinkajax.php
@@ -0,0 +1,223 @@
+
+ <?php
+
+
+ include_once 'connection.php'; 
+ 
+ 
+ if(isset($_POST['check'])) {
+ 
+ 
+ 
+ 
+   $sql="select * from panchayat WHERE Taluk_Id = ".$_POST['taluk']."  ";
+ 
+   $query =mysqli_query($conn,$sql);
+ 
+   if(mysqli_num_rows($query) > 0 ) {
+ 
+     echo "2";
+ 
+   } else
+   {
+     
+    $sqll="select AE_Id from ae WHERE Taluk_Name = ".$_POST['ae']."  ";
+    $queryy =mysqli_query($conn,$sql);
+  for each($check as $val)
+  {
+    $sql = "INSERT INTO pancahayat(AE_Id)VALUES ('".$_POST["$val"]."')";
+
+      //echo $sql;
+
+    if (mysqli_query($conn, $sql)) {
+     echo "1";
+   } else {
+     echo "0";
+   }
+   $conn->close();
+
+  }
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ } else  if(isset($_POST["md_Id"]) && !empty($_POST["md_Id"]))
+ {
+ 
+ 
+   $m="select * from ce WHERE MD_Id = ".$_POST['md_Id']."";
+     //echo $m;
+   $query =mysqli_query($conn,$m);
+ 
+     //Count total number of rows
+   $rowCount = mysqli_num_rows($query);
+ 
+     //Display Taluk list
+   if($rowCount > 0)
+   {
+     echo '<option value="" selected disabled>Select CE</option>';
+     while($row = $query->fetch_assoc()){ 
+       echo '<option value="'.$row['CE_Id'].'">'.$row['CE_Name'].'</option>';
+     }
+   }
+   else
+   {
+     echo '<option value="" selected disabled>CE not available</option>';
+   }
+ }
+ 
+ else  if(isset($_POST["CE"]) && !empty($_POST["CE"]))
+ {
+ 
+ 
+   $m="select * from se WHERE CE_Id = ".$_POST['CE']."";
+     //echo $m;
+   $query =mysqli_query($conn,$m);
+ 
+     //Count total number of rows
+   $rowCount = mysqli_num_rows($query);
+ 
+     //Display Taluk list
+   if($rowCount > 0)
+   {
+     echo '<option value="" selected disabled>Select SE</option>';
+     while($row = $query->fetch_assoc()){ 
+       echo '<option value="'.$row['SE_Id'].'">'.$row['SE_Name'].'</option>';
+     }
+   }
+   else
+   {
+     echo '<option value="" selected disabled>SE not available</option>';
+   }
+ }
+ 
+ else  if(isset($_POST["SE"]) && !empty($_POST["SE"]))
+ {
+ 
+ 
+   $m="select * from ee WHERE SE_Id = ".$_POST['SE']."";
+     //echo $m;
+   $query =mysqli_query($conn,$m);
+ 
+     //Count total number of rows
+   $rowCount = mysqli_num_rows($query);
+ 
+     //Display Taluk list
+   if($rowCount > 0)
+   {
+     echo '<option value="" selected disabled>Select EE</option>';
+     while($row = $query->fetch_assoc()){ 
+       echo '<option value="'.$row['EE_Id'].'">'.$row['EE_Name'].'</option>';
+     }
+   }
+   else
+   {
+     echo '<option value="" selected disabled>EE not available</option>';
+   }
+ }
+ 
+ else  if(isset($_POST["EE"]) && !empty($_POST["EE"]))
+ {
+ 
+ 
+   $m="select * from axe WHERE EE_Id = ".$_POST['EE']."";
+     //echo $m;
+   $query =mysqli_query($conn,$m);
+ 
+     //Count total number of rows
+   $rowCount = mysqli_num_rows($query);
+ 
+     //Display Taluk list
+   if($rowCount > 0)
+   {
+     echo '<option value="" selected disabled>Select AXE</option>';
+     while($row = $query->fetch_assoc()){ 
+       echo '<option value="'.$row['AXE_Id'].'">'.$row['AXE_Name'].'</option>';
+     }
+   }
+   else
+   {
+     echo '<option value="" selected disabled>AXE not available</option>';
+   }
+ }
+ 
+ else  if(isset($_POST["AXE"]) && !empty($_POST["AXE"]))
+ {
+ 
+ 
+   $m="select * from ae WHERE AXE_Id = ".$_POST['AXE']."";
+     //echo $m;
+   $query =mysqli_query($conn,$m);
+ 
+     //Count total number of rows
+   $rowCount = mysqli_num_rows($query);
+ 
+     //Display Taluk list
+   if($rowCount > 0)
+   {
+     echo '<option value="" selected disabled>Select AE</option>';
+     while($row = $query->fetch_assoc()){ 
+       echo '<option value="'.$row['AE_Id'].'">'.$row['AE_Name'].'</option>';
+     }
+   }
+   else
+   {
+     echo '<option value="" selected disabled>AE not available</option>';
+   }
+ }
+ 
+ 
+ 
+ else  if(isset($_POST["district_id"]) && !empty($_POST["district_id"])){
+     //Get all Taluk data
+ 
+   $m="select * from taluk WHERE District_Id = ".$_POST['district_id']."";
+     // echo $m;
+   $query =mysqli_query($conn,$m);
+ 
+     //Count total number of rows
+   $rowCount = mysqli_num_rows($query);
+ 
+     //Display Taluk list
+   if($rowCount > 0){
+     echo '<option value="" selected disabled>Select Taluk</option>';
+     while($row = $query->fetch_assoc()){ 
+       echo '<option value="'.$row['Taluk_Id'].'">'.$row['Taluk_Name'].'</option>';
+     }
+   }else{
+     echo '<option value="" selected disabled>Taluk not available</option>';
+   }
+ }
+ 
+ 
+ 
+ else  if(isset($_POST["Taluk_ID"]) && !empty($_POST["Taluk_ID"])){
+     //Get all Taluk data
+ 
+   $m="select * from panchayat WHERE Taluk_Id = ".$_POST['Taluk_ID']." AND AE_Id = 0  ";
+      // echo $m;
+   $query =mysqli_query($conn,$m);
+ 
+     //Count total number of rows
+   $rowCount = mysqli_num_rows($query);
+ 
+     //Display Taluk list
+   if($rowCount > 0){ 
+     while($row = $query->fetch_assoc()){ 
+       echo '<input type="checkbox" name="panchayat" value="'.$row['Panchayt_Id'].'" /> '.$row['Panchayt_Name'].' <br>'; 
+     }
+   }else{
+     echo '<div  >Panchayat not available</div>';
+   }
+ }
+ 
+ 
+ 
+ ?>
